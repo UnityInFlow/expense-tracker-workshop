dea996d22107b4440e4fa54b48a02feaf8a97a5b
hermanngeorge15 <hermann.george15@gmail.com>
2026-04-08 21:23:33 +0200

    Move Gradle/Maven config into session-01 as standalone project
    Students open session-01/ in IntelliJ (not root).
    Each session is self-contained with its own:
    - gradlew, gradle/wrapper/
    - build.gradle.kts, settings.gradle.kts
    - pom.xml

---
+++ session-01/settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "session-01"
+
+include(
+    "lesson-01-variables-start",
+    "lesson-01-variables-finish",
+    "lesson-02-functions-start",
+    "lesson-02-functions-finish",
+    "lesson-03-conditions-start",
+    "lesson-03-conditions-finish",
+    "lesson-04-data-class-start",
+    "lesson-04-data-class-finish",
+    "lesson-05-collections-start",
+    "lesson-05-collections-finish",
+    "lesson-06-hashmap-start",
+    "lesson-06-hashmap-finish",
+    "lesson-07-service-start",
+    "lesson-07-service-finish",
+    "lesson-08-final-start",
+    "lesson-08-final-finish",
+)
